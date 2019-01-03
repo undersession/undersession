437ea55e30ba1da355ac95ff881db038d8826fe7
Stefano Toppi <s.toppi@neobiz.it>
2019-01-03 15:56:42 +0100

    fix

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -9,6 +9,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { mobiscroll, MbscSelectOptions, MbscDatetimeOptions } from '@mobiscroll/angular';
 import { Router } from '@angular/router';
 import { EventLoggerService } from '../services/event-logger.service';
+import { ViewChild } from '@angular/core';
 
 mobiscroll.settings = {
   lang: 'it'
@@ -20,6 +21,8 @@ mobiscroll.settings = {
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+
+  @ViewChild('university')  universityRef: any;
 
   //sex
 
@@ -59,8 +62,12 @@ export class SignupPage implements OnInit {
       text: 'Bocconi'
     },
     {
-      value: 'Uni 2',
+      value: 'Uni 3',
       text: 'Cattolica'
+    },
+    {
+      value: 'todelete',
+      text: ''
     }
   ];
 
@@ -69,7 +76,23 @@ export class SignupPage implements OnInit {
     height: 50,
     data: this.university,
     filter: true,
-    inputClass: 'demo-non-form'
+    inputClass: 'demo-non-form',
+    onSet: () => {
+      this.university.pop();
+    },
+    onItemTap: (event, inst) => {
+      inst.setVal(event.value);
+      inst.select();
+    },
+    onFilter: (event) => {
+      this.university.pop();
+      this.university.push(
+        {
+          value: event.filterText,
+          text: event.filterText
+        }
+      )
+    }
   };
 
   faculty: any = [
@@ -78,16 +101,20 @@ export class SignupPage implements OnInit {
       text: ''
     },
     {
-      value: 'Uni 1',
+      value: 'Facolta 1',
       text: 'Facolta 1'
     }, 
     {
-      value: 'Uni 2',
+      value: 'Facolta 2',
       text: 'Facolta 2'
     },
     {
-      value: 'Uni 2',
-      text: 'Facolta 3'
+      value: 'Facolta 3',
+      text: 'Facolta 3',
+    },
+    {
+      value: 'todelete',
+      text: ''
     }
   ];
 
@@ -96,7 +123,23 @@ export class SignupPage implements OnInit {
     height: 50,
     data: this.faculty,
     filter: true,
-    inputClass: 'demo-non-form'
+    inputClass: 'demo-non-form',
+    onSet: () => {
+      this.faculty.pop();
+    },
+    onItemTap: (event, inst) => {
+      inst.setVal(event.value);
+      inst.select();
+    },
+    onFilter: (event) => {
+      this.faculty.pop();
+      this.faculty.push(
+        {
+          value: event.filterText,
+          text: event.filterText
+        }
+      )
+    }
   };
 
   nonFormSettingsDate: MbscDatetimeOptions = {
@@ -128,7 +171,10 @@ export class SignupPage implements OnInit {
     });
   }
 
+  
+
   ngOnInit() {
+
   }
 
   // Create user using form builder controls
